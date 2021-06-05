72a282ed7d06318dfb8b2d201f5169c913d0d3f9
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-06-05 12:30:36 -0700

    properly enabled a single socket connection for each client

--- server/index.ts
+++ server/index.ts
@@ -11,12 +11,31 @@ const io = new Server(server);
 app.use('/', express.static(path.join(__dirname, '../build')));
 
 io.on('connection', (socket) => {
+
   console.log('a user has connected');
+  socket.emit('HelloClient', getCurrentTime());
+  console.log(socket.id);
 
   socket.on('disconnect', () => {
     console.log('a user has disconnected');
   });
+
+  //listening events
+
+  //template sorta
+  socket.on('insertEventNameHere', () => {
+    //do stuff
+    let variable = 'whatever variable stuff you want';
+    socket.emit('insertClientListeningEventNameHere', variable);
+  });
 });
+
+const getCurrentTime = () => {
+  const currentDate = new Date();
+  const dateTime = currentDate.getDate() + '/' + (currentDate.getMonth() +1) + '/' + currentDate.getFullYear() + ' ' + currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds();
+  console.log(dateTime)
+  return dateTime;
+};
 
 server.listen(port, ()=> {
   console.log(`listening on port ${port}`);
--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,10 +1,33 @@
-import React, {FC} from 'react'
-import '../sass/styles.scss'
+import React, { FC, useState, useEffect } from 'react';
+import '../sass/styles.scss';
+import socketIOClient from 'socket.io-client';
+// const ENDPOINT = '';
 
 const App:FC = () => {
+  const [connection, setConnection] = useState({});
+  const [response, setResponse] = useState(''); //socket setup only //not important
+
+  useEffect(() => {
+    console.log('connecting')
+    // const socket = socketIOClient(ENDPOINT);
+    const socket = socketIOClient();
+    setConnection(socket);
+    socket.on('HelloClient', data => {
+      setResponse(data);
+    });
+  }, []);
   return (
-    <div>placeholder text</div>
+    <div>
+      <div>socket info:
+        <br />
+        socket ID: {connection.id}
+        <br/>
+        connection time: {response}
+      </div>
+
+      <div>placeholder text</div>
+    </div>
   )
 }
 
-export default App
+export default App;
