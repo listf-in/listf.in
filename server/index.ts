65832284f83651c4b1a50e4288fd6a85219e5603
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-06-05 11:14:26 -0700

    Implemented serverside initial socket setup

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,24 @@
-import * as path from 'path'
-import * as express from 'express'
-const port = 3080
-const app = express()
+import * as path from 'path';
+import * as express from 'express';
+const port = 3080;
+const app = express();
+import * as http from 'http';
+const server = http.createServer(app);
+import { Server } from 'socket.io';
+const io = new Server(server);
 
-app.use('/', express.static(path.join(__dirname, '../build')))
 
-app.listen(port, ()=> {
-  console.log(`listening on port ${port}`)
-})
+app.use('/', express.static(path.join(__dirname, '../build')));
+
+io.on('connection', (socket) => {
+  console.log('a user has connected');
+
+  socket.on('disconnect', () => {
+    console.log('a user has disconnected');
+  });
+});
+
+server.listen(port, ()=> {
+  console.log(`listening on port ${port}`);
+});
+
