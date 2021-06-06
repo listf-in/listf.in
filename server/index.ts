2f13dd12dba7457268b4089a58e4f3763ef862d4
tarrinneal <tarrinneal@gmail.com>
2021-06-05 17:29:53 -0700

    gets dummy data and puts in on the screen in string format

--- server/index.ts
+++ server/index.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '../build')));
 
 app.get('/board', (req, res) => {
+  debugger;
   res.send(boardData);
 });
 
--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,14 +1,16 @@
 import React, { FC, useState, useEffect } from 'react';
 import '../sass/styles.scss';
 import { io, Socket } from 'socket.io-client';
-import { Method } from 'axios';
+import axios, { Method } from 'axios';
 // const ENDPOINT = ''; //if we use a specific endpoint
 
-const App:FC = () => {
+const App: FC = () => {
   const [connection, setConnection] = useState<null | Socket>(null);
   // const [connection, setConnection] = useState(null as null | Socket); //alternative typing
 
   const [connectionTime, setConnectionTime] = useState(''); //example
+
+  const [boards, setBoards] = useState({});
 
   useEffect(() => {
     console.log('connecting');
@@ -20,20 +22,31 @@ const App:FC = () => {
     socket.on('HelloClient', (data: string) => {
       setConnectionTime(data);
     });
+    axios
+      .get('/board')
+      .then((boards) => {
+        debugger;
+        setBoards(boards.data);
+      })
+      .catch((err) => {
+        debugger;
+        setBoards(err);
+      });
   }, []);
 
   return (
     <div>
-      <div>socket info:
+      <div>
+        socket info:
         <br />
         socket ID: {connection ? connection.id : null}
-        <br/>
+        <br />
         connection time: {connectionTime}
       </div>
 
-      <div>placeholder text</div>
+      <div>{JSON.stringify(boards)}</div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
