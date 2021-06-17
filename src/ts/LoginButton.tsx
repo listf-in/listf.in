657d868c28321bb5af80ecc9a1f3f5b573c58f8e
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-06-17 14:38:27 -0700

    Logout button implemented (ugly implementation)

--- src/ts/LoginButton.tsx
+++ src/ts/LoginButton.tsx
@@ -2,8 +2,12 @@ import React, {FC} from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton: FC = () => {
-  const { loginWithRedirect } = useAuth0()
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0()
 
+  // If we're already logged in display logout button instead:
+  if (isAuthenticated) {
+    return <button onClick={() => logout({ returnTo: window.location.origin })}></button>
+  }
   return <button onClick={() => loginWithRedirect()}>Log in</button>;
 }
 
