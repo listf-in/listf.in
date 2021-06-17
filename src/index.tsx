fa8b20fb70d65c6142fcaffa73e3641f1e09e3f8
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-06-17 14:24:14 -0700

    LoginButton component

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,11 @@ import App from './ts/App'
 import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <Auth0Provider >
+  <Auth0Provider
+    domain='dev-v191p81s.us.auth0.com'
+    clientId='QBalhUxGPNfswO2xamB6QHoDx3QXdRFa'
+    redirectUri={window.location.origin}
+  >
     <App />
   </Auth0Provider>,
   document.getElementById('root')
