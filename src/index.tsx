b2fe7974b7697652a1afd28f6aca5797b82ac4bf
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-06-17 14:20:33 -0700

    Installing and initializing Auth0

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './ts/App'
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Auth0Provider >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+)
