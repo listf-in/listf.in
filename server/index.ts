8159910c4771e252be3015852883499c919e42d1
tarrinneal <tarrinneal@gmail.com>
2021-07-09 22:39:56 -0700

    attempt to fix __dirname bug in deploy

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,12 @@
 import * as path from 'path';
 import * as express from 'express';
-import { Socket } from 'socket.io';
+__dirname = __dirname || path.resolve();
 const boardData = require('../dummy_board_data.json');
 
 const port = process.env.PORT || 3080;
 const app = express();
 
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '../build')));
@@ -16,28 +15,6 @@ app.get('/board', (req, res) => {
   res.send(boardData);
 });
 
-io.on('connection', (socket: Socket) => {
-  console.log('a user has connected, socket.id: ', socket.id);
-  socket.emit('HelloClient', new Date().toLocaleString());
-
-  socket.on('disconnect', () => {
-    console.log('a user has disconnected, socket.id: ', socket.id);
-  });
-
-  //listening events
-
-  socket.on('insertEventNameHere', () => {
-    //template sorta
-    //do stuff
-    const variable: string = 'whatever variable stuff you want';
-    socket.emit('insertClientListeningEventNameHere', variable);
-  });
-});
-
-/*const server = */ http.listen(port, () => {
+http.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
-
-// httpServer.listen(port, ()=> {
-//   console.log(`listening on port ${port}`);
-// });
