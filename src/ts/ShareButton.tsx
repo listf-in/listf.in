3a2d7858b1946ead6191811dc496072b261a70b4
tarrinneal <tarrinneal@gmail.com>
2021-07-14 02:50:51 -0700

    added share functionality in it's most pimitive form

---
+++ src/ts/ShareButton.tsx
@@ -0,0 +1,27 @@
+import { ApolloClient, gql, NormalizedCacheObject } from '@apollo/client';
+import React, { FC } from 'react';
+import '../sass/styles.scss';
+
+type shareButtonProps = {
+  id: string;
+};
+
+const ShareButton: FC<shareButtonProps> = ({ id }) => {
+  const [code, setCode] = React.useState('');
+  return (
+    <div>
+      <i
+        className='fas fa-share-alt-square shareButton shareButtons clickable'
+        onClick={() => {
+          setCode(id);
+          setTimeout(() => {
+            setCode('');
+          }, 5000);
+        }}
+      ></i>
+      <span className='shareCode clickable'>{code}</span>
+    </div>
+  );
+};
+
+export default ShareButton;
