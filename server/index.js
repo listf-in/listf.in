e56c0ac5673fde0b779cf4f90d13312718ce4a37
tarrinneal <tarrinneal@gmail.com>
2021-08-01 17:42:33 -0700

    initialized apollo server with initial schema ret up'

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 /* eslint-disable no-undef */
 const path = require('path');
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 
-const port = process.env.PORT || 3080;
-const app = express();
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  const app = express();
+  server.applyMiddleware({ app });
+  app.use(express.json());
+  app.use('/', express.static(path.join(__dirname, '../build')));
+  await new Promise((resolve) => app.listen({ port: 3080 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:3080${server.graphqlPath}`);
+}
+startApolloServer(require('../schemagql.graphql'));
 
-const http = require('http').Server(app);
+// const port = process.env.PORT || 3080;
+// const app = express();
 
-app.use(express.json());
-app.use('/', express.static(path.join(__dirname, '../build')));
+// const http = require('http').Server(app);
 
-http.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+// http.listen(port, () => {
+//   console.log(`listening on port ${port}`);
+// });
