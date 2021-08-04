9c7d0e26087d552315945c92940a690e464615dd
tarrinneal <tarrinneal@gmail.com>
2021-08-03 23:25:52 -0700

    more npm packs for subs

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 /* eslint-disable no-undef */
-const path = require('path');
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+// const path = require('path');
+// const express = require('express');
+// const { ApolloServer } = require('apollo-server-express');
 
-async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({ typeDefs, resolvers });
-  await server.start();
-  const app = express();
-  server.applyMiddleware({ app });
-  app.use(express.json());
-  app.use('/', express.static(path.join(__dirname, '../build')));
-  await new Promise((resolve) => app.listen({ port: 3080 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:3080${server.graphqlPath}`);
-}
-startApolloServer(require('../schemagql.graphql'));
+// async function startApolloServer(typeDefs, resolvers) {
+//   const server = new ApolloServer({ typeDefs, resolvers });
+//   await server.start();
+//   const app = express();
+//   server.applyMiddleware({ app });
+//   app.use(express.json());
+//   app.use('/', express.static(path.join(__dirname, '../build')));
+//   await new Promise((resolve) => app.listen({ port: 3080 }, resolve));
+//   console.log(`🚀 Server ready at http://localhost:3080${server.graphqlPath}`);
+// }
+// startApolloServer(require('../schemagql.graphql'));
 
 // const port = process.env.PORT || 3080;
 // const app = express();
@@ -23,3 +23,36 @@ startApolloServer(require('../schemagql.graphql'));
 // http.listen(port, () => {
 //   console.log(`listening on port ${port}`);
 // });
+
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { createServer } = require('http');
+const { execute, subscribe } = require('graphql');
+const { PubSub } = require('graphql-subscriptions');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
+const { myGraphQLSchema } = require('../schemagql.graphql');
+
+const PORT = 3080;
+const app = express();
+
+app.use('/graphql', express.json());
+
+const apolloServer = new ApolloServer({ schema: myGraphQLSchema });
+apolloServer.applyMiddleware({ app });
+
+const pubsub = new PubSub();
+const server = createServer(app);
+
+server.listen(PORT, () => {
+  new SubscriptionServer(
+    {
+      execute,
+      subscribe,
+      schema: myGraphQLSchema,
+    },
+    {
+      server: server,
+      path: '/subscriptions',
+    }
+  );
+});
