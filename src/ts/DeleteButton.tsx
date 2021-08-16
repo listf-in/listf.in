b06d31333df9015b09d2b30f684c5daf681af9e8
tarrinneal <tarrinneal@gmail.com>
2021-08-16 15:08:14 -0700

    altered delete function to only remove from parent

--- src/ts/DeleteButton.tsx
+++ src/ts/DeleteButton.tsx
@@ -4,11 +4,17 @@ import '../sass/styles.scss';
 
 type DeleteButtonProps = {
   boardID: string;
+  parentID: string;
   callback: Function;
   client: ApolloClient<NormalizedCacheObject>;
 };
 
-const DeleteButton: FC<DeleteButtonProps> = ({ boardID, client, callback }) => {
+const DeleteButton: FC<DeleteButtonProps> = ({
+  boardID,
+  parentID,
+  client,
+  callback,
+}) => {
   const deleteFromParentBoard = (
     e: React.MouseEvent<HTMLElement, MouseEvent>,
     boardID: string
@@ -17,12 +23,20 @@ const DeleteButton: FC<DeleteButtonProps> = ({ boardID, client, callback }) => {
     client
       .mutate({
         mutation: gql`mutation{
-          deleteBoard(filter: {
-            id: "${boardID}"
-          }) {
+          updateBoard(input: {
+           filter: {
+              id: "${parentID}"
+            },
+            remove: {
+              listItems: {
+                id: "${boardID}"
+              }
+            }
+
+        }){
             board {
-              name
               id
+              name
             }
           }
         }
@@ -45,3 +59,18 @@ const DeleteButton: FC<DeleteButtonProps> = ({ boardID, client, callback }) => {
 };
 
 export default DeleteButton;
+
+// former delete function
+// .mutate({
+//   mutation: gql`mutation{
+//     deleteBoard(filter: {
+//       id: "${boardID}"
+//     }) {
+//       board {
+//         name
+//         id
+//       }
+//     }
+//   }
+//   `,
+// })
