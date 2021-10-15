5365dcbb5b75350f383735bd6f3933546a1d9492
tarrinneal <tarrinneal@gmail.com>
2021-10-15 15:04:42 -0700

    its a start

--- src/ts/Landing.tsx
+++ src/ts/Landing.tsx
@@ -6,6 +6,9 @@ const Landing: FC = () => {
   return (
     <div id='landing'>
       <h1 className='landingLogo'>LISTF.IN</h1>
+      <h3 className='landingSubtitle'>
+        Infinite Lists, Infinite Possibilities
+      </h3>
       <LoginButton />
     </div>
   );
