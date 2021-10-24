278adfa423fd53c6dd0e283ba6383373ade3b24e
tarrinneal <tarrinneal@gmail.com>
2021-10-23 20:52:42 -0700

    less header

--- src/ts/Header.tsx
+++ src/ts/Header.tsx
@@ -2,13 +2,13 @@ import React, { FC } from 'react';
 import '../sass/styles.scss';
 
 import LoginButton from './LoginButton';
-import ProfileInfo from './ProfileInfo';
+// import ProfileInfo from './ProfileInfo';
 
 const Header: FC = () => {
   return (
     <div id='header'>
       <h1 className='logo'>LISTF∞IN</h1>
-      <ProfileInfo />
+      {/* <ProfileInfo /> */}
       <div id='loginButton'>
         <LoginButton />
       </div>
